c8b32dc748095f86e56b02b74ed8653a74dfd794
Samfouss <samfouss192@gmail.com>
2022-08-15 22:21:19 -0400

    Correction de la matrice construite entre les CRHs et les cellules

---
+++ scripts/cells_clustering.R
@@ -0,0 +1,20 @@
+# Chargement de packages
+library("FactoMineR")
+library("factoextra")
+
+cells_data <- cells_matrix
+cells_data[cells_data == 1] <- "in"
+cells_data[cells_data == 0] <- "Not in"
+
+cells_data <- data.frame(cells_data)
+
+res.mca <- MCA(cells_data, graph = FALSE)
+print(res.mca)
+eig.val <- get_eigenvalue(res.mca)
+
+fviz_mca_biplot(res.mca, repel = TRUE, ggtheme = theme_minimal())
+fviz_mca_var(res.mca, choice = "mca.cor", repel = TRUE)
+fviz_mca_var(res.mca, repel = TRUE, ggtheme= theme_minimal())
+fviz_mca_var(res.mca, choice = "quanti.sup", ggtheme = theme_minimal())
+fviz_mca_ind(res.mca, label = "ind.sup", ggtheme = theme_minimal())
+
--- scripts/cells_matrix.R
+++ scripts/cells_matrix.R
@@ -1,12 +1,24 @@
+library(tidyverse)
 
+load("rdata/all_net_result.rda")
+result <- all_net_result$block1$resume_fusion%>%
+  str_split("-", simplify = TRUE)
+
+result <- gsub(",.*","",result)
+result <- gsub(".* ","",result)
+
+view(result)
 
 crhs_fus_len = nrow(result)
 
 cells_matrix = matrix(
   0,
-  nrow = 250,
+  nrow = crhs_fus_len,
   ncol = 250,
-  dimnames = list(str_c("cell", sprintf("%03d", 1:250)), str_c("cell", sprintf("%03d", 1:250)))
+  dimnames = list(
+    str_c("crhs", sprintf("%03d", 1:crhs_fus_len)), 
+    str_c("cell", sprintf("%03d", 1:250))
+  )
 )
 
 for(r in seq_len(crhs_fus_len)){
@@ -16,15 +28,13 @@ for(r in seq_len(crhs_fus_len)){
   
   # Comme on ne compte qu'une seule fois les cellules meme si les deux chromosomes entrent dans la fusion, alors il nous suffit d'effacer les duplicate et de construire une matrice d'incidence à ajouter a la matrice finale "cells_matrix"
   extract_cells = extract_cells[!duplicated(extract_cells)]
-  nb_extract_cells = length(extract_cells)
   
   cells_matrix[
-    str_c("cell", extract_cells),
+    r,
     str_c("cell", extract_cells)
-  ] <- cells_matrix[str_c("cell", extract_cells), str_c("cell", extract_cells)] + 1
+  ] <- 1
 }
 
-diag(cells_matrix) <- 0
 view(cells_matrix)
 
 save(cells_matrix, file = "rdata/cells_matrix.rda")
