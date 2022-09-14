0970f915dbada6e3f98c03e1dfd30bdd95e700c6
abureau <abureau6@gmail.com>
2022-09-14 10:10:35 -0400

    Ajout des instructions de clustering

--- scripts/cells_clustering.R
+++ scripts/cells_clustering.R
@@ -17,4 +17,12 @@ fviz_mca_var(res.mca, choice = "mca.cor", repel = TRUE)
 fviz_mca_var(res.mca, repel = TRUE, ggtheme= theme_minimal())
 fviz_mca_var(res.mca, choice = "quanti.sup", ggtheme = theme_minimal())
 fviz_mca_ind(res.mca, label = "ind.sup", ggtheme = theme_minimal())
+fviz_mca_ind(res.mca, col.ind = "cos2", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE, ggtheme = theme_minimal())
+hcpc_cluster <- HCPC(res.mca, kk=Inf, min = 3, graph = FALSE)
+# Le dendogram
+fviz_dend(hcpc_cluster, cex = 0.7, palette = "jco", rect = TRUE, rect_fill = TRUE, rect_border = "jco")
+# Graphique
+fviz_cluster(hcpc_cluster, repel = TRUE, geom = "point", main = "Classification des cellules")
+table(hcpc_cluster$data.clust$clust)
+hcpc_cluster$call$t
 
