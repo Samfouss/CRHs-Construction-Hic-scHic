042551f41efe22ea717d80eb53395656e5d04b4c
abureau <abureau6@gmail.com>
2022-09-14 12:40:50 -0400

    Calcul du nombre de CRHs par cluster

--- scripts/CRHs_compte_clusters.R
+++ scripts/CRHs_compte_clusters.R
@@ -8,3 +8,12 @@ CRHs_compte_clusters(cells_data$crhs001,hcpc_cluster$data.clust$clust)
 
 nclusters_CRHs = apply(cells_data,2,CRHs_compte_clusters,clusters=hcpc_cluster$data.clust$clust)
 table(nclusters_CRHs)
+
+# Nombre de CRHs différents par cluster
+ncl = nlevels(hcpc_cluster$data.clust$clust)
+nCRHs_par_cluster = numeric(ncl)
+for (i in 1:ncl)
+{
+  iCRH = apply(cells_data[hcpc_cluster$data.clust$clust==i,],2,function(vec) any(vec=="in"))
+  nCRHs_par_cluster[i] = sum(iCRH)
+}
