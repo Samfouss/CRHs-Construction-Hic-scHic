6665168dc8037563f8b967ed49c9ff278234334a
abureau <abureau6@gmail.com>
2022-09-19 10:08:20 -0400

    Examine CRHs avec un nombre minimal de répétitions

--- scripts/CRHs_compte_clusters.R
+++ scripts/CRHs_compte_clusters.R
@@ -3,11 +3,25 @@ CRHs_compte_clusters = function(cel,clusters)
 {
   length(unique(clusters[cel=="in"]))
 }
+CRHs_compte_clusters_tm = function(cel,clusters,tm)
+{
+  sum(table(clusters[cel=="in"])>=tm)
+}
+
 # Test
-CRHs_compte_clusters(cells_data$crhs001,hcpc_cluster$data.clust$clust)
+CRHs_compte_clusters(cells_data[,1],hcpc_cluster$data.clust$clust)
+CRHs_compte_clusters_tm(cells_data[,1],hcpc_cluster$data.clust$clust,1)
 
 nclusters_CRHs = apply(cells_data,2,CRHs_compte_clusters,clusters=hcpc_cluster$data.clust$clust)
 table(nclusters_CRHs)
+summary(nclusters_CRHs)
+
+nclusters_CRHs = apply(cells_data_,2,CRHs_compte_clusters,clusters=hcpc_cluster_$data.clust$clust)
+table(nclusters_CRHs)
+
+nclusters_CRHst5 = apply(cells_data,2,CRHs_compte_clusters_tm,clusters=hcpc_cluster$data.clust$clust,tm=5)
+table(nclusters_CRHst5)
+summary(nclusters_CRHst5[nclusters_CRHst5>0])
 
 # Nombre de CRHs différents par cluster
 ncl = nlevels(hcpc_cluster$data.clust$clust)
@@ -17,3 +31,16 @@ for (i in 1:ncl)
   iCRH = apply(cells_data[hcpc_cluster$data.clust$clust==i,],2,function(vec) any(vec=="in"))
   nCRHs_par_cluster[i] = sum(iCRH)
 }
+nCRHs_par_cluster
+summary(nCRHs_par_cluster)
+
+# Nombre de clusters avec au moins tm répétitions de chaque CRH
+tm=5
+nCRHst5_par_cluster = numeric(ncl)
+for (i in 1:ncl)
+{
+  iCRH = apply(cells_data[hcpc_cluster$data.clust$clust==i,],2,function(vec) sum(vec=="in")>=tm)
+  nCRHst5_par_cluster[i] = sum(iCRH)
+}
+nCRHst5_par_cluster
+summary(nCRHst5_par_cluster)
