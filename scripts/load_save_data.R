b74aee24f933bd15e050b159cbb4ebb1749a6811
Samfouss <samfouss192@gmail.com>
2022-12-19 12:28:44 -0500

    Suppression des données sur la vérification et la compréhension des données simulées et conservation des données sur le scénario 0 dans le fichier rdata\single_cell_hic_data

--- scripts/load_save_data.R
+++ scripts/load_save_data.R
@@ -22,29 +22,38 @@ load("rdata/all_single_structure.rda")
 load("rdata/all_paired_structure.rda")
 
 structure_1 <- as_tibble(
-  all_single_structure%>%
-  filter(replica == sprintf("%03d", 1))%>%
-  select(-c(ends_with("_c"), "replica"))%>%
-  mutate(
-    ID = paste0("B", sprintf("%02d", X4), sprintf("%04d", 1:n()))
-  )
+  all_paired_structure%>%
+    filter(paire == str_c(1, sprintf("%03d", 1)))%>%
+    select(-c(ends_with("_c"), "paire"))%>%
+    mutate(
+      ID = paste0("B", sprintf("%02d", X4), sprintf("%04d", 1:n()))
+    )
 )
 
 structure_2 <- as_tibble(
-  all_single_structure%>%
-  filter(replica == sprintf("%03d", 2))%>%
-  select(-c(ends_with("_c"), "replica"))%>%
-  mutate(
-    ID = paste0("B", sprintf("%02d", X4), sprintf("%04d", 1:n()))
-  )
+  all_paired_structure%>%
+    filter(paire == str_c(2, sprintf("%03d", 1)))%>%
+    select(-c(ends_with("_c"), "paire"))%>%
+    mutate(
+      ID = paste0("B", sprintf("%02d", X4), sprintf("%04d", 1:n()))
+    )
 )
 
 structure_3 <- as_tibble(
-  all_single_structure%>%
-  filter(replica == sprintf("%03d", 3))%>%
-  select(-c(ends_with("_c"), "replica"))%>%
-  mutate(
-    ID = paste0("B", sprintf("%02d", X4), sprintf("%04d", 1:n()))
-  )
+  all_paired_structure%>%
+    filter(paire == str_c(1, sprintf("%03d", 2)))%>%
+    select(-c(ends_with("_c"), "paire"))%>%
+    mutate(
+      ID = paste0("B", sprintf("%02d", X4), sprintf("%04d", 1:n()))
+    )
+)
+
+structure_4 <- as_tibble(
+  all_paired_structure%>%
+    filter(paire == str_c(2, sprintf("%03d", 2)))%>%
+    select(-c(ends_with("_c"), "paire"))%>%
+    mutate(
+      ID = paste0("B", sprintf("%02d", X4), sprintf("%04d", 1:n()))
+    )
 )
 
