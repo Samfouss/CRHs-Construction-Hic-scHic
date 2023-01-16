a84ab944d9285562b07d9f23a08e43802a4154c7
Samfouss <samfouss192@gmail.com>
2023-01-16 09:57:28 -0500

    Finalisation de la fonction converting_matrix dans le fichier scripts/clustering/convert_matrix_Higachi_fn.R pour avoir les données acceptées par le programme de Higachi et sauvegarde des résultats sur 100 cellules

--- scripts/clustering/convert_matrix_Higachi_fn.R
+++ scripts/clustering/convert_matrix_Higachi_fn.R
@@ -1,7 +1,8 @@
-library(vroom)
+library(data.table)
+library(tidyverse)
 
 # To charge large dataset
-# higachi_data <- fread("../Higashi/data/data.txt")
+higachi_data <- fread("../Higashi/data/data.txt")
 # nb_cells = 10
 
 # Cette onction est créée pour convertir le fichier des données scHic simulées en fichier accepté par le programme HIgachi
@@ -17,75 +18,72 @@ library(vroom)
 # 4: ML1_GCCTCGAA_GAGTACGT       3   chr1 209393848   chr1 232468122     1
 # 5: ML1_GCTCGCTA_CTAGTGAA       4   chr5   5565796   chr5 149955845     1
 # 6: ML1_CAGGCTTG_GATATAAC       5   chr1 181632778   chr1 181731968     1
-converting_matrix <- function(nb_cells, fenetre){
-  
-  # On charge une première matrice dont le nombre de lignes nous servira par la suite
-  mat <- as.matrix(
-    read.table(
-      paste0("rdata/single_cell_hic_data/hic_mat_", sprintf("%03d", i), ".txt"), 
-      quote="\"", 
-      comment.char="", 
-      stringsAsFactors = FALSE
-    ) 
+
+# Une bille fait 2667 bases
+converting_matrix <- function(nb_cells, nb_mat_line){
+  # Nombre de bases que représente une bille dans les polymeres
+  bins_equiv = 2667
+
+  cell_dim = nb_mat_line*(nb_mat_line-1)/2
+  nb_rows = cell_dim*nb_cells
+
+  # 'cell_id', 'chrom1', 'pos1', 'chrom2', 'pos2', 'count'
+  cell_data_higachi <- matrix(
+    NA,
+    nrow = nb_rows, 
+    ncol = 6
   )
   
-  max_dim = nrow(mat)
-  r = ceiling(max_dim/fenetre)
-  # 
-  if(max_dim<fenetre){
-    
-    cat("SVP vous devez donner une fênetre inférieur à la dimenssion de la matrice")
+  #cell_data_higachi = as.data.frame(cell_data_higachi)
   
-  }else{
-    # 'cell_id', 'chrom1', 'pos1', 'chrom2', 'pos2', 'count'
-    cell_data_higachi <- matrix(
-      NA,
-      nrow = 2*r*nb_cells, 
-      ncol = 6
+  for (i in seq_len(nb_cells)) {
+    
+    mat <- as.matrix(
+      read.table(
+        paste0("rdata/single_cell_hic_data/hic_mat_", sprintf("%03d", i), ".txt"), 
+        quote="\"", 
+        comment.char="", 
+        stringsAsFactors = FALSE
+      ) 
     )
     
-    for (i in seq_len(nb_cells)) {
+    block_cell = cell_dim*(i-1)
+    # Initialisation de l'index de la ligne
+    line = 0
+    
+    for (chr1 in seq_len(nb_mat_line)) {
+      # pos1 = ceiling(((chr1-1)*bins_equiv + chr1*bins_equiv)/2)
+      pos1 = ceiling(bins_equiv*(chr1-1) + bins_equiv/2)
       
-      mat <- as.matrix(
-        read.table(
-          paste0("rdata/single_cell_hic_data/hic_mat_", sprintf("%03d", i), ".txt"), 
-          quote="\"", 
-          comment.char="", 
-          stringsAsFactors = FALSE
-        ) 
-      )
-      
-      for (chr1 in (seq_len(r)-1)) {
-        chr1_pos1 = chr1*fenetre + 1
-        chr1_pos2 = (chr1+1)*fenetre
-        if(chr1_pos2>max_dim){
-          chr1_pos2 = max_dim
-        }
-        for (chr2 in (seq_len(r)-1)) {
-          chr2_pos1 = chr2*fenetre + 1
-          chr2_pos2 = (chr1+1)*fenetre
-          if(chr2_pos2>max_dim){
-            chr2_pos2 = max_dim
-          }
-          
-          # cell_data_higachi[r*r*(i-1) + (r*chr1+(chr2 + 1)), 1] <- paste("cell_", i-1)
-          # cell_data_higachi[r*r*(i-1) + (r*chr1+(chr2 + 1)), 2] <- i-1
-          # cell_data_higachi[r*r*(i-1) + (r*chr1+(chr2 + 1)), 3] <- paste("chr", chr1+1)
-          # cell_data_higachi[r*r*(i-1) + (r*chr1+(chr2 + 1)), 4] <- chr1_pos2
-          # cell_data_higachi[r*r*(i-1) + (r*chr1+(chr2 + 1)), 5] <- paste("chr", chr1+1)
-          # cell_data_higachi[r*r*(i-1) + (r*chr1+(chr2 + 1)), 6] <- 
-          # cell_data_higachi[r*r*(i-1) + (r*chr1+(chr2 + 1)), 7] <- 
+      for (chr2 in seq_len(nb_mat_line)) {
+        # Ici, il nous faut juste prendre la partie supérieure (ou inférieure), la diagonale exlue de la matrice des contacts
+        # Donc pour une ligne donnée, on prend les céllules dont l'index de la colonne est superieur à celui de la ligne en question
+        if(chr2 > chr1){
+          pos2 = ceiling(bins_equiv*(chr2-1) + bins_equiv/2)
+          line = line + 1
+
+          cell_data_higachi[block_cell+line, 1] <- i-1
+          cell_data_higachi[block_cell+line, 2] <- "chr1"
+          cell_data_higachi[block_cell+line, 3] <- pos1
+          cell_data_higachi[block_cell+line, 4] <- "chr2"
+          cell_data_higachi[block_cell+line, 5] <- pos2
+          cell_data_higachi[block_cell+line, 6] <- mat[chr1, chr2]
         }
       }
-      
-    } 
+    }
+    
   }
+
+  colnames(cell_data_higachi) <- c('cell_id', 'chrom1', 'pos1', 'chrom2', 'pos2', 'count')
+  cell_data_higachi
   
 }
 
 
+cell_data_higachi <- converting_matrix(100, 562)
 
+# Save promoters ID
+save(cell_data_higachi, file = "rdata/cell_data_higachi.rda")
 
-
-
+# write.table(cell_data_higachi, file = "rdata/data.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
 
