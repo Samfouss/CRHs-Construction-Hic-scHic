b971f6d8b90bb07a8e08d65a7b7e7149d97a68ae
Samfouss <samfouss192@gmail.com>
2023-05-26 15:18:04 -0400

    Correction du code sur la detection des CRHs qui se chevauche puis sauvegarde des résultats dans le fichier rdata/crhs_struc_fusion.rda

--- scripts/crhs_creation_with_bins/cells_clustering_crhs_less_complex.R
+++ scripts/crhs_creation_with_bins/cells_clustering_crhs_less_complex.R
@@ -23,21 +23,36 @@ for (i in 2:length(all_net_result_complex)) {
 # Compter le nombre de CRHs retenu
 
 nb_crhs = 0
+block_len = 0
 for (bl in 2:16) {
+  b = 0
   for (i in seq_len(length(all_net_result_complex[[bl]]$crhs))) {
+    
     if(length(all_net_result_complex[[bl]]$crhs[[i]])>1){
       
-      nb_crhs = nb_crhs + 1
+      b = b + 1
     }
   }
+  
+  nb_crhs = nb_crhs + b
+  block_len = block_len + b*b
 }
 
 nb_crhs
+block_len
 
 # Est ce qu'un CRHs est inclus dans l'autre après degenerescence ?
-
+load("rdata/all_net_result_complex.rda")
 # Etape 3 : fonction permettant de faire la réduction de matrices
 source("scripts/crhs_comparaison/degenerationMatrix_fn.R")
+
+crhs_struc_fusion = matrix(
+  0,
+  nrow = block_len,
+  ncol = 4
+)
+
+l = 1
 
 for (bl in 2:16) {
   complex_crh = vector(mode = "numeric", length = length(all_net_result_complex[[bl]]$crhs))
@@ -56,13 +71,13 @@ for (bl in 2:16) {
     
     for (j in complex_crh) {
       
-      if(i != j){
+      if(i > j){
         
         # Initilisation de la premières matrice d'incidence parmi les CRHs complexes
-        mat_inc_init = all_net_result_complex[[bl]]$crhs[[complex_crh[i]]]
+        mat_inc_init = all_net_result_complex[[bl]]$crhs[[i]]$mat_incidence
         mat_inc_init_deg = degenerationMatrix(mat_inc_init)
         
-        mat_inc = all_net_result_complex[[bl]]$crhs[[j]]
+        mat_inc = all_net_result_complex[[bl]]$crhs[[j]]$mat_incidence
         mat_inc_deg = degenerationMatrix(mat_inc)
         
         # redimenssion des deux matrices afin de calcluer les statistiques sur les intersections
@@ -80,11 +95,14 @@ for (bl in 2:16) {
         mat_deg_redim1 <- mat_inc_deg[rowmat, colmat]
         mat_deg_redim2 <- mat_inc_init_deg[rowmat, colmat]
         
-        if((max(sum(mat_deg_redim1==1), sum(mat_deg_redim2==1))) != 0){
-          intersection = sum(mat_deg_redim1[mat_deg_redim2==mat_deg_redim1]==1)/(max(sum(mat_deg_redim1==1), sum(mat_deg_redim2==1)))
-          if(intersection==1){
-            print(intersection)
-          }
+        if((min(sum(mat_deg_redim1==1), sum(mat_deg_redim2==1))) != 0){
+          intersection = sum(mat_deg_redim1[mat_deg_redim2==mat_deg_redim1]==1)/(min(sum(mat_deg_redim1==1), sum(mat_deg_redim2==1)))
+          # Sauvegarde des données
+          crhs_struc_fusion[l, 1] = bl
+          crhs_struc_fusion[l, 2] = i
+          crhs_struc_fusion[l, 3] = j
+          crhs_struc_fusion[l, 4] = intersection
+          l = l + 1
         }
       }
       
@@ -93,6 +111,11 @@ for (bl in 2:16) {
   
 }
 
+crhs_struc_fusion = crhs_struc_fusion[crhs_struc_fusion[, 1]!= 0, ]
+
+summary(crhs_struc_fusion[, 4])
+
+save(crhs_struc_fusion, file = "rdata/crhs_struc_fusion.rda")
 ########### Sauvegarde des données ###########
 save(all_net_result_complex, file = "rdata/all_net_result_complex.rda")
 
